39257f1ff985219bc652ac2472b76bcac3f92658
ZebraVogel94349 <cowo2312@gmail.com>
2022-09-11 00:09:49 +0200

    added draw_field() and draw_board()

---
+++ src/gfx/colors.h
@@ -0,0 +1,6 @@
+const int BLACK = 0;
+const int BACKGROUND_YELLOW = 1;
+const int RED = 2;
+const int BLUE = 3;
+const int GREEN = 4;
+const int YELLOW = 5;
--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,37 @@
 #include <fileioc.h>
 #include <graphx.h>
 
+#include "gfx/palette_gfx.h"
+#include "gfx/colors.h"
+
+void draw_field(int x, int y, int color){
+    gfx_SetColor(BLACK);
+    gfx_FillCircle_NoClip(x + 10, y + 10, 10);
+    gfx_SetColor(color);
+    gfx_FillCircle_NoClip(x + 10, y + 10, 9);
+}
+
+void draw_board(){
+    int board[1][3] = {{0, 0, BLUE}};
+    for(int i = 0; i < 1; i++){
+        draw_field(board[i][0], board[i][1], board[i][2]);
+    }
+}
+
 int main(){
     gfx_Begin();
+    gfx_SetPalette(palette_gfx, sizeof_palette_gfx, 0);
     gfx_SetDrawBuffer();
+    gfx_FillScreen(1);
+    gfx_SwapDraw();
+    gfx_FillScreen(1);
+
+    //Main Loop
+    while(kb_Data[6] != kb_Clear){
+        draw_board();
+        gfx_SwapDraw();
+        kb_Scan();
+    }
     gfx_End();
+    
 }

0c490267e759c2efaf545e23f48a841a6579a828
einsmaxe <maximilianflenker@gmail.com>
2022-09-11 02:02:12 +0200

    added board

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,32 @@
 
 #include "gfx/palette_gfx.h"
 #include "gfx/colors.h"
+#include "gfx/vars.h"
 
 void draw_field(int x, int y, int color){
     gfx_SetColor(BLACK);
-    gfx_FillCircle_NoClip(x + 10, y + 10, 10);
+    gfx_FillCircle_NoClip(x + 10, y + 10, 8);
     gfx_SetColor(color);
-    gfx_FillCircle_NoClip(x + 10, y + 10, 9);
+    gfx_FillCircle_NoClip(x + 10, y + 10, 7);
 }
 
 void draw_board(){
-    int board[1][3] = {{0, 0, BLUE}};
-    for(int i = 0; i < 1; i++){
+    int board[72][3] = {{left_margin, top_margin, GREEN}, {left_margin + 21, top_margin, GREEN}, {left_margin, top_margin + 21, GREEN}, {left_margin + 21, top_margin + 21, GREEN}, 
+                       {left_margin + 189, top_margin, BLUE}, {left_margin + 210, top_margin, BLUE}, {left_margin + 189, top_margin + 21, BLUE}, {left_margin + 210, top_margin + 21, BLUE},
+                       {left_margin + 189, top_margin + 189, RED}, {left_margin + 210, top_margin + 189, RED}, {left_margin + 189, top_margin + 210, RED}, {left_margin + 210, top_margin + 210, RED},
+                       {left_margin, top_margin + 189, YELLOW}, {left_margin + 21, top_margin + 189, YELLOW}, {left_margin, top_margin + 210, YELLOW}, {left_margin + 21, top_margin + 210, YELLOW},
+                       {left_margin + 84, top_margin, BACKGROUND_YELLOW}, {left_margin + 105, top_margin, BACKGROUND_YELLOW}, {left_margin + 126, top_margin, BLUE},
+                       {left_margin + 84, top_margin + 21, BACKGROUND_YELLOW}, {left_margin + 105, top_margin + 21, BLUE}, {left_margin + 126, top_margin + 21, BACKGROUND_YELLOW},
+                       {left_margin + 84, top_margin + 42, BACKGROUND_YELLOW}, {left_margin + 105, top_margin + 42, BLUE}, {left_margin + 126, top_margin + 42, BACKGROUND_YELLOW},
+                       {left_margin + 84, top_margin + 63, BACKGROUND_YELLOW}, {left_margin + 105, top_margin + 63, BLUE}, {left_margin + 126, top_margin + 63, BACKGROUND_YELLOW},
+                       {left_margin, top_margin + 84, GREEN}, {left_margin + 21, top_margin + 84, BACKGROUND_YELLOW}, {left_margin + 42, top_margin + 84, BACKGROUND_YELLOW}, {left_margin + 63, top_margin + 84, BACKGROUND_YELLOW}, {left_margin + 84, top_margin + 84, BACKGROUND_YELLOW}, {left_margin + 105, top_margin + 84, BLUE}, {left_margin + 126, top_margin + 84, BACKGROUND_YELLOW}, {left_margin + 147, top_margin + 84, BACKGROUND_YELLOW}, {left_margin + 168, top_margin + 84, BACKGROUND_YELLOW}, {left_margin + 189, top_margin + 84, BACKGROUND_YELLOW}, {left_margin + 210, top_margin + 84, BACKGROUND_YELLOW},
+                       {left_margin, top_margin + 105, BACKGROUND_YELLOW}, {left_margin + 21, top_margin + 105, GREEN}, {left_margin + 42, top_margin + 105, GREEN}, {left_margin + 63, top_margin + 105, GREEN}, {left_margin + 84, top_margin + 105, GREEN}, {left_margin + 126, top_margin + 105, RED}, {left_margin + 147, top_margin + 105, RED}, {left_margin + 168, top_margin + 105, RED}, {left_margin + 189, top_margin + 105, RED}, {left_margin + 210, top_margin + 105, BACKGROUND_YELLOW},
+                       {left_margin, top_margin + 126, BACKGROUND_YELLOW}, {left_margin + 21, top_margin + 126, BACKGROUND_YELLOW}, {left_margin + 42, top_margin + 126, BACKGROUND_YELLOW}, {left_margin + 63, top_margin + 126, BACKGROUND_YELLOW}, {left_margin + 84, top_margin + 126, BACKGROUND_YELLOW}, {left_margin + 105, top_margin + 126, YELLOW}, {left_margin + 126, top_margin + 126, BACKGROUND_YELLOW}, {left_margin + 147, top_margin + 126, BACKGROUND_YELLOW}, {left_margin + 168, top_margin + 126, BACKGROUND_YELLOW}, {left_margin + 189, top_margin + 126, BACKGROUND_YELLOW}, {left_margin + 210, top_margin + 126, RED},
+                       {left_margin + 84, top_margin + 147, BACKGROUND_YELLOW}, {left_margin + 105, top_margin + 147, YELLOW}, {left_margin + 126, top_margin + 147, BACKGROUND_YELLOW},
+                       {left_margin + 84, top_margin + 168, BACKGROUND_YELLOW}, {left_margin + 105, top_margin + 168, YELLOW}, {left_margin + 126, top_margin + 168, BACKGROUND_YELLOW},
+                       {left_margin + 84, top_margin + 189, BACKGROUND_YELLOW}, {left_margin + 105, top_margin + 189, YELLOW}, {left_margin + 126, top_margin + 189, BACKGROUND_YELLOW},
+                       {left_margin + 84, top_margin + 210, YELLOW}, {left_margin + 105, top_margin + 210, BACKGROUND_YELLOW}, {left_margin + 126, top_margin + 210, BACKGROUND_YELLOW}};
+    for(int i = 0; i < 72; i++){
         draw_field(board[i][0], board[i][1], board[i][2]);
     }
 }

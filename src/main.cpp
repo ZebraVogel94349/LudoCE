e22c1bc999f2d7a4a38ec2b0f3f2cb5d79de1ffa
einsmaxe <maximilianflenker@gmail.com>
2022-09-18 20:05:16 +0200

    optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,12 @@
 #include <fileioc.h>
 #include <graphx.h>
 
+#include <debug.h>
+
 #include "gfx/palette_gfx.h"
 #include "gfx/colors.h"
 #include "const.h"
+
 
 void draw_field(int x, int y, int r, int color){
     gfx_SetColor(BLACK);
@@ -39,16 +42,24 @@ void draw_board(){
 }
 
 void redraw_field(int position){
-    if(position == 0){
-        draw_field(BOARD[0][0], BOARD[0][1], 8, BOARD[0][2]);
-    }
-    else{
-        draw_field(BOARD[position][0], BOARD[position][1], 8, BOARD[position][2]);
-    }
+    draw_field(BOARD[position][0], BOARD[position][1], 8, BOARD[position][2]);
 }
 
-void draw_player(int color, int position){
-    draw_field(BOARD[position][0], BOARD[position][1], 5, color);
+void draw_player(int playerPositions[], int start, int end){
+    for(int i = start; i < end; i++){
+        if(i >= BOARD_DATA.GREEN.playerNumberStart){
+            draw_field(BOARD[playerPositions[i]][0], BOARD[playerPositions[i]][1], 5, GREEN);
+        }
+        else if(i >= BOARD_DATA.YELLOW.playerNumberStart){
+            draw_field(BOARD[playerPositions[i]][0], BOARD[playerPositions[i]][1], 5, YELLOW);
+        }
+        else if(i >= BOARD_DATA.RED.playerNumberStart){
+            draw_field(BOARD[playerPositions[i]][0], BOARD[playerPositions[i]][1], 5, RED);
+        }
+        else{
+            draw_field(BOARD[playerPositions[i]][0], BOARD[playerPositions[i]][1], 5, BLUE);
+        }
+    }
 }
 
 int is_occupied(int position, int playerPositions[]){
@@ -65,35 +76,31 @@ int move_n_fields(int piece_color, int player, int position, int n){
 
     switch (piece_color){
         case BLUE:
-            hm_pos = BOARD_DATA.BLUE.hm_pos + player;
             startPoint = BOARD_DATA.BLUE.startPoint;
             h_size = BOARD_DATA.BLUE.h_size;
             h_offset = BOARD_DATA.BLUE.h_offset;
             h_start = BOARD_DATA.BLUE.h_start;
             break;
         case RED:
-            hm_pos = BOARD_DATA.RED.hm_pos + player;
             startPoint = BOARD_DATA.RED.startPoint;
             h_size = BOARD_DATA.RED.h_size;
             h_offset = BOARD_DATA.RED.h_offset;
             h_start = BOARD_DATA.RED.h_start;
             break;
         case YELLOW:
-            hm_pos = BOARD_DATA.YELLOW.hm_pos + player;
             startPoint = BOARD_DATA.YELLOW.startPoint;
             h_size = BOARD_DATA.YELLOW.h_size;
             h_offset = BOARD_DATA.YELLOW.h_offset;
             h_start = BOARD_DATA.YELLOW.h_start;
             break;
         case GREEN:
-            hm_pos = BOARD_DATA.GREEN.hm_pos + player;
             startPoint = BOARD_DATA.GREEN.startPoint;
             h_size = BOARD_DATA.GREEN.h_size;
             h_offset = BOARD_DATA.GREEN.h_offset;
             h_start = BOARD_DATA.GREEN.h_start;
             break;
     }
-    if(position == hm_pos && n == 6){//move out of home
+    if(position >= 56 && n == 6){//move out of home
         return startPoint;
     }
     else if(position <= h_offset && position + n > h_offset && position + n <= h_offset + h_size){//move into house
@@ -115,20 +122,39 @@ int move_n_fields(int piece_color, int player, int position, int n){
     else{
         return position;
     }
+}
 
-    /*// throw out or scip player 
+int *move_player(int playerPositions[], int piece_color, int player, int old_position, int n){
+    int new_position = move_n_fields(piece_color, player, old_position, n);
+
     for(int i = 0; i < BOARD_DATA.player_count; i++){
-        if(PLAYER[playerNumber] == PLAYER[i] && (i < playerNumberStart || i > playerNumberStart + 3)){
-            PLAYER[i] = BOARD_DATA.BLUE.hm_pos + i;
-            
-            // redraw player in home
-            draw_player(i);
-            break;
-        }else if(PLAYER[playerNumber] == PLAYER[i] && i >= playerNumberStart && i <= playerNumberStart + 3 && i != playerNumberStart + player){
-            PLAYER[playerNumber] = position; // dont move
-            break;
+        if(new_position == playerPositions[i] && (i < player / BOARD_DATA.BLUE.h_size || i > player / BOARD_DATA.BLUE.h_size + 3)){
+            playerPositions[i] = BOARD_DATA.BLUE.hm_pos + i;
+            dbg_printf("TEST");
+            return playerPositions;
+        }else if(new_position == playerPositions[i] && i >= player / BOARD_DATA.BLUE.h_size && i <= player / BOARD_DATA.BLUE.h_size + 3 && i != player){
+            playerPositions[player] = old_position; // dont move
+            dbg_printf("TEST");
+            return playerPositions;
+        }else{
+            playerPositions[player] = new_position;
         }
-    }*/
+    }
+    
+    return playerPositions;
+}
+
+void draw(int playerPositions[], int toClear){
+    draw_player(playerPositions, 0, BOARD_DATA.player_count);
+    if(!is_occupied(toClear, playerPositions)){
+        redraw_field(toClear);
+    }
+    gfx_SwapDraw();
+    draw_player(playerPositions, 0, BOARD_DATA.player_count);
+    if(!is_occupied(toClear, playerPositions)){
+        redraw_field(toClear);
+    }
+    dbg_printf("%d\n", playerPositions[5]);
 }
 
 int main(){
@@ -142,61 +168,36 @@ int main(){
 
     gfx_FillScreen(BACKGROUND_YELLOW);
     draw_board();
-
-    for(int i = 0; i < BOARD_DATA.player_count; i++){//Draw all players
-        if(i >= BOARD_DATA.GREEN.playerNumberStart){
-            draw_player(GREEN, playerPositions[i]);
-        }
-        else if(i >= BOARD_DATA.YELLOW.playerNumberStart){
-            draw_player(YELLOW, playerPositions[i]);
-        }
-        else if(i >= BOARD_DATA.RED.playerNumberStart){
-            draw_player(RED, playerPositions[i]);
-        }
-        else{
-            draw_player(BLUE, playerPositions[i]);
-        }
-    }
+    draw_player(playerPositions, 0, BOARD_DATA.player_count);
     gfx_SwapDraw();
 
 
     gfx_FillScreen(BACKGROUND_YELLOW);
     draw_board();
-    for(int i = 0; i < BOARD_DATA.player_count; i++){//Draw all players
-        if(i >= BOARD_DATA.GREEN.playerNumberStart){
-            draw_player(GREEN, playerPositions[i]);
-        }
-        else if(i >= BOARD_DATA.YELLOW.playerNumberStart){
-            draw_player(YELLOW, playerPositions[i]);
-        }
-        else if(i >= BOARD_DATA.RED.playerNumberStart){
-            draw_player(RED, playerPositions[i]);
-        }
-        else{
-            draw_player(BLUE, playerPositions[i]);
-        }
-    }
+    draw_player(playerPositions, 0, BOARD_DATA.player_count);
 
+    int player = 0;
+
+    toClear = playerPositions[4];
+    *playerPositions = *move_player(playerPositions, RED, 4, playerPositions[4], 6);
+    *playerPositions = *move_player(playerPositions, RED, 4, playerPositions[4], 2);
+    draw(playerPositions, toClear);
+    toClear = playerPositions[5];
+    *playerPositions = *move_player(playerPositions, RED, 5, playerPositions[5], 6);
+    *playerPositions = *move_player(playerPositions, RED, 5, playerPositions[5], 10);
+    draw(playerPositions, toClear);
 
     //Main Loop
     while(kb_Data[6] != kb_Clear){
         //Testing the move_n_fields() function
         for(int i = 0; i < 7 && kb_Data[6] != kb_Clear; i++){
-            
-            toClear = playerPositions[0];
-            playerPositions[0] = move_n_fields(BLUE, 0, playerPositions[0], (rand() % 6) + 1);
 
-            draw_player(BLUE, playerPositions[0]);
-            if(!is_occupied(toClear, playerPositions)){
-                redraw_field(toClear);
-            }
+            /*toClear = playerPositions[player];
+            *playerPositions = *move_player(playerPositions, BLUE, player, playerPositions[player], 6);
+            draw(playerPositions, toClear);*/
             gfx_SwapDraw();
-            draw_player(BLUE, playerPositions[0]);
-            if(!is_occupied(toClear, playerPositions)){
-                redraw_field(toClear);
-            }
             
-            msleep(200);
+            msleep(500);
             kb_Scan();
         }
     }

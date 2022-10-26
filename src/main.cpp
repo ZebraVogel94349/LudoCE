f902102eb97159dec4acd4c7fc5b74375bb4f2aa
ZebraVogel94349 <cowo2312@gmail.com>
2022-10-26 17:27:12 +0200

    removed unnecessary includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,7 @@
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
 #include <tice.h>
-
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include <keypadc.h>
 #include <fileioc.h>
 #include <graphx.h>
-#include <debug.h>
 
 #include "gfx/palette_gfx.h"
 #include "gfx/colors.h"
